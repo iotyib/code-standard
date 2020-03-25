5261f31b60c20198c69e70851b247d126902fbd2
yib <iotyib@163.com>
2020-03-25 14:08:51 +0800

    TCP socket code

---
+++ tcp socket/tcp_client.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef __TCP_CLIENT_H__
+#define __TCP_CLIENT_H__
+
+
+int tcp_client();
+
+#endif // !__TCP_CLIENT_H__
+
---
+++ tcp socket/tcp_server.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef __TCP_SERVER_H__
+#define __TCP_SERVER_H__
+
+int tcp_server();
+
+#endif // !__TCP_SERVER_H__
+
+
